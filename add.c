7a2d2282b067c12ec20d8d23038d69c8ac634abc
Brandon Murry <brandonfossedu@fcboe.org>
2022-10-26 14:33:36 -0400

    Modified add.c

--- add.c
+++ add.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-    printf("sum = %i\n", num1 + num2);
+    if(argv[1] != NULL &&  argv[2] != NULL) {
+        int num1 = atoi(argv[1]);
+        int num2 = atoi(argv[2]);
+        printf("sum = %i\n", num1 + num2);
+    }
+    else
+        printf("You need to enter 2 numbers\n");
     return 0;
 }
