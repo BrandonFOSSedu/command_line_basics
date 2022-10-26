b065a919fa6a18df596e30dedea5aec6e383f83f
Brandon Murry <brandonfossedu@fcboe.org>
2022-10-26 10:11:12 -0400

    Initial C programs

---
+++ add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    printf("sum = %i\n", num1 + num2);
+    return 0;
+}
---
+++ arguments.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("You typed %i arguments", argc);
+    return 0;
+}
